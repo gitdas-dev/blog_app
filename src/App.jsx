7497a800ca1cc1861c42fd016fd45f66840c1a72
Mandeep Das <mandeepdas321@gmail.com>
2024-08-15 00:31:31 +0530

    3rd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
+import { useState, useEffect } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from './Appwrite/Auth'
+import store from './store/store'
+import { login, logout } from './store/authSlice'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>A blog app with appwrite</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login(userData))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => {
+      setLoading(false)
+    })
+  
+    
+  }, [])
+  
+  return !loading ? (
+    <div className='min-h-sc flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>{/* outlet */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : (null)
 }
 
 export default App
---
+++ src/components/Select.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Select({
+    options,
+    label,
+    className,
+    ...props
+}) {
+  return (
+    <div>Select</div>
+  )
+}
+
+export default Select
